48859afc05c8cfeabbd53f84d020cd613b617f99
RAGURAM-S <raguramsathasivam@gmail.com>
2024-10-22 22:14:38 +0530

    added support for otel metrics

--- app/go.mod
+++ app/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gofiber/contrib/otelfiber v1.0.10 // indirect
 	github.com/gofiber/fiber/v2 v2.52.5 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.22.0 // indirect
@@ -14,19 +15,22 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.opentelemetry.io/contrib v1.17.0 // indirect
 	go.opentelemetry.io/contrib/bridges/otelslog v0.6.0 // indirect
 	go.opentelemetry.io/otel v1.31.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc v0.7.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v1.31.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.31.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.31.0 // indirect
 	go.opentelemetry.io/otel/log v0.7.0 // indirect
 	go.opentelemetry.io/otel/metric v1.31.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.31.0 // indirect
 	go.opentelemetry.io/otel/sdk/log v0.7.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v1.31.0 // indirect
 	go.opentelemetry.io/otel/trace v1.31.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.3.1 // indirect
 	golang.org/x/net v0.30.0 // indirect
--- app/main.go
+++ app/main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"context"
 	"log/slog"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"go.opentelemetry.io/contrib/bridges/otelslog"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/log/global"
+	otelmetric "go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/log"
+	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 	oteltrace "go.opentelemetry.io/otel/trace"
 )
@@ -26,10 +30,17 @@ func newLogExporter(ctx context.Context) (log.Exporter, error) {
 	return otlploggrpc.New(ctx)
 }
 
-func newTraceProvider(r *resource.Resource, exp sdktrace.SpanExporter) *sdktrace.TracerProvider {
-	return sdktrace.NewTracerProvider(
-		sdktrace.WithBatcher(exp),
-		sdktrace.WithResource(r),
+func newMetricExporter(ctx context.Context) (metric.Exporter, error) {
+	return otlpmetricgrpc.New(
+		ctx,
+		otlpmetricgrpc.WithCompressor("gzip"),
+	)
+}
+
+func newTraceProvider(r *resource.Resource, exp trace.SpanExporter) *trace.TracerProvider {
+	return trace.NewTracerProvider(
+		trace.WithBatcher(exp),
+		trace.WithResource(r),
 	)
 }
 
@@ -40,10 +51,68 @@ func newLogProvider(r *resource.Resource, logExporter log.Exporter) *log.LoggerP
 	)
 }
 
+func newMeterProvider(r *resource.Resource, metricExporter metric.Exporter, metricFrequency time.Duration) *metric.MeterProvider {
+	meterProvider := metric.NewMeterProvider(
+		metric.WithResource(r),
+		metric.WithReader(metric.NewPeriodicReader(metricExporter,
+			// Default is 1m
+			metric.WithInterval(metricFrequency))),
+	)
+	return meterProvider
+}
+
+const (
+	metricNameHttpServerDuration       = "http.server.duration"
+	metricNameHttpServerRequestSize    = "http.server.request.size"
+	metricNameHttpServerResponseSize   = "http.server.response.size"
+	metricNameHttpServerActiveRequests = "http.server.active_requests"
+
+	UnitDimensionless = "1"
+	UnitBytes         = "By"
+	UnitMilliseconds  = "ms"
+)
+
 var (
 	Tracer oteltrace.Tracer
 	Logger *slog.Logger
+	Meter  otelmetric.Meter
 )
+
+type config struct {
+	Next              func(*fiber.Ctx) bool
+	TracerProvider    oteltrace.TracerProvider
+	MeterProvider     otelmetric.MeterProvider
+	Port              *int
+	Propagators       propagation.TextMapPropagator
+	ServerName        *string
+	SpanNameFormatter func(*fiber.Ctx) string
+}
+
+// func metricsTrackingMiddleware(meter otelmetric.Meter) fiber.Handler {
+// 	httpServerDuration, err := meter.Float64Histogram(metricNameHttpServerDuration, otelmetric.WithUnit(UnitMilliseconds), otelmetric.WithDescription("measures the duration inbound HTTP requests"))
+// 	if err != nil {
+// 		otel.Handle(err)
+// 	}
+// 	httpServerRequestSize, err := meter.Int64Histogram(metricNameHttpServerRequestSize, otelmetric.WithUnit(UnitBytes), otelmetric.WithDescription("measures the size of HTTP request messages"))
+// 	if err != nil {
+// 		otel.Handle(err)
+// 	}
+// 	httpServerResponseSize, err := meter.Int64Histogram(metricNameHttpServerResponseSize, otelmetric.WithUnit(UnitBytes), otelmetric.WithDescription("measures the size of HTTP response messages"))
+// 	if err != nil {
+// 		otel.Handle(err)
+// 	}
+// 	httpServerActiveRequests, err := meter.Int64UpDownCounter(metricNameHttpServerActiveRequests, otelmetric.WithUnit(UnitDimensionless), otelmetric.WithDescription("measures the number of concurrent HTTP requests that are currently in-flight"))
+// 	if err != nil {
+// 		otel.Handle(err)
+// 	}
+// 	return func(c *fiber.Ctx) error {
+// 		defer func() {
+
+// 		}()
+
+// 		return nil
+// 	}
+// }
 
 func main() {
 	ctx := context.Background()
@@ -59,6 +128,7 @@ func main() {
 		panic(err)
 	}
 
+	// set up tracer
 	traceExporter, err := newTraceExporter(ctx)
 	if err != nil {
 		panic(err)
@@ -68,8 +138,11 @@ func main() {
 	defer traceProvider.Shutdown(ctx)
 
 	otel.SetTracerProvider(traceProvider)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+
 	Tracer = traceProvider.Tracer("test-app-tracer")
 
+	// set up log tracking
 	logExporter, err := newLogExporter(ctx)
 	if err != nil {
 		panic(err)
@@ -82,7 +155,21 @@ func main() {
 
 	Logger = otelslog.NewLogger("test-app-logger")
 
+	// set up metrics tracking
+	metricExporter, err := newMetricExporter(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	meterProvider := newMeterProvider(r, metricExporter, 1*time.Minute)
+	defer meterProvider.Shutdown(ctx)
+
+	otel.SetMeterProvider(meterProvider)
+
+	Meter = otel.Meter("test-app-metrics")
+
 	app := fiber.New()
+	// app.Use(metricsTrackingMiddleware)
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		ctx, span := Tracer.Start(c.Context(), "hello-world-span")
